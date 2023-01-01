21e3f5262ab84ebad2133f2e8568946e6a4ff3e1
Pureharish <108225503+pureharish@users.noreply.github.com>
2023-01-01 16:24:17 +0530

    few modifications

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,26 @@
 let one = document.getElementById("number_one");
 let result = document.getElementById("result_text");
 let all_btn = Array.from(document.getElementsByClassName("number_btn"));
-one.addEventListener("click", one_click);
-function one_click(e) {
-  result.textContent += "1";
-}
+let add = document.getElementById('add');
+let sub = document.getElementById('sub');
+let multiply = document.getElementById('multiply');
+let divide = document.getElementById('divide');
+let a = [];
+let number1;
 
 all_btn.forEach((Element) => {
   Element.addEventListener("click", (buttons_clicked) => {
-    result.textContent += `${Element.innerHTML}`;
+    result.textContent += `${Element.textContent}`;
+    console.log(Element)
+    a.push(`${Element.textContent}`);
+    number1 = result.textContent;
+    console.log(number1);
   });
 });
+
+
+
+
+
 
 
