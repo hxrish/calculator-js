f8997a1a99acccc5f5c555803aaeb23cac165ff1
Pureharish <108225503+pureharish@users.noreply.github.com>
2023-01-03 22:01:04 +0530

    Made the numbers and operators, functional

--- javascript/script.js
+++ javascript/script.js
@@ -3,9 +3,33 @@ let operators = Array.from(document.getElementsByClassName('operatorBTN'));
 let number1 = document.getElementById('number1');
 let operationDisplay = document.getElementById('operation');
 let number2 = document.getElementById('number2');
+let specialBTN = Array.from(document.getElementsByClassName('specialBTN'));
+
+
+function clear(){
+    number1.innerText = '';
+    number2.innerText = '';
+}
 
 numbers.forEach(Element => {
     Element.addEventListener('click', elementsClicked => {
         number1.innerText += Element.innerText;
+    })
+})
+
+operators.forEach(Element => {
+    Element.addEventListener('click', () => {
+        operationDisplay.innerHTML = `<p id="operationNow">${Element.innerText}</p>`;
+    })
+})
+
+specialBTN.forEach(Element => {
+    Element.addEventListener('click', () => {
+        if (Element.innerText == 'C'){
+            number1.innerText = number1.innerText.slice(0, -1);
+        }
+        if (Element.innerText == 'AC'){
+            clear()
+        }
     })
 })
