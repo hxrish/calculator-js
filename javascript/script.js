6292c6cf3b8b445bfb85567b21a37f429c02c7f7
Pureharish <108225503+pureharish@users.noreply.github.com>
2023-01-30 05:01:42 +0530

    .

--- javascript/script.js
+++ javascript/script.js
@@ -13,89 +13,60 @@ function clear() {
   operationDisplay.innerText = "";
 }
 
-
 numbers.forEach((Element) => {
   Element.addEventListener("click", (elementsClicked) => {
     number1.innerText += Element.innerText;
   });
 });
 
-decimal.addEventListener('click', () => {
-  let decimalplace = '.';
-  if(number1.innerText.indexOf(decimalplace) < 0){
-    number1.innerText = `${number1.innerText}.`
+decimal.addEventListener("click", () => {
+  let decimalplace = ".";
+  if (number1.innerText.indexOf(decimalplace) < 0) {
+    number1.innerText = `${number1.innerText}.`;
   }
-  
-})
-
+});
 
 operators.forEach((Element) => {
   Element.addEventListener("click", () => {
-    if(operationDisplay.innerText == ''){
-    operationDisplay.innerHTML = `<p id="operationNow">${Element.innerText}</p>`;
+    if (operationDisplay.innerText === "") {
+      operationDisplay.innerHTML = `<p id="operationNow">${Element.innerText}</p>`;
 
-    operation(operationDisplay.innerText)
-  }
+      operation(operationDisplay.innerText);
+    }
   });
-
 });
 
-
-function operation(operator){
-
+function operation(operator) {
   let number1num = parseFloat(number1.innerText);
   let number2num = parseFloat(number2.innerText);
 
   switch (operator) {
-    case '+':
-      if(number2.innerText == ''){
+    case "+":
+      if (number2.innerText === "") {
         number2.innerText = number1.innerText;
-        number1.innerText = ''
-      }
-      else{
-        // number1.innerText = number2num + number1num;
-        // number2.innerText = '';
+        number1.innerText = "";
       }
       break;
 
-      case '-':
-        if(number2.innerText == ''){
-          number2.innerText = number1.innerText;
-          number1.innerText = ''
-        }
-        else{
-          // number1.innerText = number2num - number1num;
-          // number2.innerText = '';
-        }
-        break;
+    case "-":
+      if (number2.innerText === "") {
+        number2.innerText = number1.innerText;
+        number1.innerText = "";
+      }
+      break;
 
-        case '*':
-          if(number2.innerText == ''){
-            number2.innerText = number1.innerText;
-            number1.innerText = ''
-          }
-          else{
-            // number1.innerText = number2num * number1num;
-            // number2.innerText = '';
-          }
-          break;
+    case "*":
+      if (number2.innerText === "") {
+        number2.innerText = number1.innerText;
+        number1.innerText = "";
+      }
+      break;
 
-          case '/':
-            if(number2.innerText == ''){
-              number2.innerText = number1.innerText;
-              number1.innerText = ''
-            }
-
-            // else if(number2.innerText != '' && number1.innerText == 0){
-            //   number1.innerText  = 'Illegal!';
-            //   number2.innerText = '';
-            // }
-
-            // else{
-            //   number1.innerText = number2num / number1num;
-            //   number2.innerText = '';
-            // }
-            // break;
+    case "/":
+      if (number2.innerText === "") {
+        number2.innerText = number1.innerText;
+        number1.innerText = "";
+      }
     default:
       break;
   }
@@ -103,57 +74,53 @@ function operation(operator){
 
 specialBTN.forEach((Element) => {
   Element.addEventListener("click", () => {
-    if (Element.innerText == "C") {
+    if (Element.innerText === "C") {
       number1.innerText = number1.innerText.slice(0, -1);
     }
-    if (Element.innerText == "AC") {
+    if (Element.innerText === "AC") {
       clear();
     }
   });
 });
 
-
-equal.addEventListener('click', () => {
-  if(number2.innerText == ''){
-    return
-  }
-  else{
-  let number1num = parseFloat(number1.innerText);
-  let number2num = parseFloat(number2.innerText);
+equal.addEventListener("click", () => {
+  if (number2.innerText === "") {
+    return;
+  } else {
+    let number1num = parseFloat(number1.innerText);
+    let number2num = parseFloat(number2.innerText);
     switch (operationDisplay.innerText) {
-      case '+':
+      case "+":
         number1.innerText = number2num + number1num;
-        number2.innerText = '';
-        operationDisplay.innerText = '';
-        
+        number2.innerText = "";
+        operationDisplay.innerText = "";
+
         break;
 
-      case '-':
+      case "-":
         number1.innerText = number2num - number1num;
-        number2.innerText = '';
-        operationDisplay.innerText = '';
+        number2.innerText = "";
+        operationDisplay.innerText = "";
 
         break;
 
-      case '*':
+      case "*":
         number1.innerText = number2num * number1num;
-        number2.innerText = '';
+        number2.innerText = "";
 
         break;
 
-      case '/':
-        if(number2.innerText != '' && number1.innerText == 0){
-          number1.innerText  = 'Illegal!';
-          number2.innerText = '';
-          operationDisplay.innerText = '';
-        }
-
-        else{
+      case "/":
+        if (number2.innerText != "" && number1.innerText === 0) {
+          number1.innerText = "Illegal!";
+          number2.innerText = "";
+          operationDisplay.innerText = "";
+        } else {
           number1.innerText = number2num / number1num;
-          number2.innerText = '';
-          operationDisplay.innerText = '';
+          number2.innerText = "";
+          operationDisplay.innerText = "";
         }
         break;
-        }
+    }
   }
-})
+});
